1031fa9b53dfd6f1b553efe8364dbe5b5d29d4f8
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-08-30 00:12:46 -0500

    Added functionally to create a sketchpad based a given size from user, sizes range from 16x16 grid to 100x100 grid

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const sketchpad = document.getElementById("sketchpad");
 
 
-let sketchsize = 16 * 16;
-let count = 0;
 
-for (let i = 0; i < sketchsize; i++) {
-    count = count + 1;
-    const space = document.createElement("div");
-    space.classList.add("sketchspace");
-    sketchpad.appendChild(space);
+function buildPad(size) {
+    let sketchsize = size * size;
+    let count = 0;
+    for (let i = 0; i < sketchsize; i++) {
+        count = count + 1;
+        const space = document.createElement("div");
+        space.classList.add("sketchspace");
+        sketchpad.appendChild(space);
+    }
+    console.log(count);
 }
+
+buildPad(16);
+
+const sketchspaces = document.querySelectorAll("div.sketchspace");
+sketchspaces.forEach(space => space.addEventListener("mouseover", colorInSpace));
+
+
+function resizeSketchPad() {
+    const sketchpadsize = document.getElementById("sketchpadsize");
+    sketchpad.innerHTML = "";
+    sketchpad.style.gridTemplateColumns = `repeat(${sketchpadsize.value}, calc(500px/${sketchpadsize.value}))`;
+    sketchpad.style.gridTemplateRows = `repeat(${sketchpadsize.value}, calc(500px/${sketchpadsize.value}))`;
+    buildPad(sketchpadsize.value);
+    const sketchspaces = document.querySelectorAll("div.sketchspace");
+    sketchspaces.forEach(space => space.style.height = `calc(500px/${sketchpadsize.value})`);
+    sketchspaces.forEach(space => space.style.width = `calc(500px/${sketchpadsize.value})`);
+    sketchspaces.forEach(space => space.addEventListener("mouseover", colorInSpace));
+}
+
+const resizebutton = document.getElementById("submit");
+resizebutton.onclick = resizeSketchPad;
 
 function colorInSpace(e) {
     e.target.classList.add("blackspace");
@@ -21,8 +45,6 @@ function resetSketchpad() {
     console.log("reset");
 }
 
-const sketchspaces = document.querySelectorAll("div.sketchspace");
-sketchspaces.forEach(space => space.addEventListener("mouseover", colorInSpace));
 
 const resetbutton = document.getElementById("reset");
 resetbutton.onclick = resetSketchpad;
